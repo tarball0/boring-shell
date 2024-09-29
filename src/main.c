1d2a00eab37a084bf69a09dfe4d2325ebbf4deee
tarball0 <chrispaul.tar.gz@proton.me>
2024-09-29 22:11:24 +0530

    improved formatting

--- src/main.c
+++ src/main.c
@@ -73,17 +73,28 @@ int getArgs(char line_in[], char *charray_out[]) {
 
 int changedir(const char path[]) {
     int result;
-    asm("syscall" : "=a"(result) : "a"(80), "D"(path) : "rcx", "r11", "memory");
+    asm ("syscall" 
+		: "=a"(result) 
+		: "a"(80), "D"(path) 
+		: "rcx", "r11", "memory"
+		);
+
     /* rcx and r11 are always clobbered by the syscall
      * The syscall instruction uses rcx to store the address of the next
      * instruction to return to, and r11 to save the value of the rflags
      * register. These values will then be restored by the sysret instruction.
      */
+
     return result;
 }
 
 int getpwd(char *pwd) {
-    asm("syscall" : : "a"(79), "D"(pwd), "S"(64) : "rcx", "r11", "memory");
+    asm ("syscall" 
+		: 
+		: "a"(79), "D"(pwd), "S"(64) 
+		: "rcx", "r11", "memory"
+		);
+
     return 0;
 }
 
