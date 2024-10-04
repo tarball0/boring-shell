7d1f6eb5bff5b1a4f45f01257c44a648131f9b2c
tarball0 <chrispaul.tar.gz@proton.me>
2024-10-04 10:05:16 +0530

    added tab completion and history

--- src/main.c
+++ src/main.c
@@ -1,13 +1,17 @@
 /*
  * the boring-shell, a random sunday project :)
- * I know this is nothing special (quite boring), although I am slowly escaping tutorial hell.
- * also, inline assembly is fun
+ * I know this is nothing special (quite boring), although I am slowly escaping
+ * tutorial hell. also, inline assembly is fun
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <sys/wait.h>
+#include <unistd.h>
+
+#include <readline/history.h>
+#include <readline/readline.h>
 
 /* Escape Sequence that clears the terminal */
 #define clear() printf("\033[H\033[J")
@@ -20,22 +24,28 @@ int execute(char *, char **);
 
 /* main :D */
 int main(int argc, char *argv[]) {
-    char prompt[] = ">>> ";
+    char prompt[] = " >>> ";
     char *commandarr[32];
-    char command[2048];
+    char command[2048] = "";
+    char *command_buf = "";
     char exe[32], pwd[64];
     int len;
-    while (strcmp(command, "exit") != 0) {
-        if (strcmp(command, "clear") == 0) {
-            clear();
-        }
+    while (strcmp(exe, "exit") != 0) {
         getpwd(pwd);
-        printf("%s %s", pwd, prompt);
-        if (fgets(command, sizeof(command), stdin) == 0) {
+        strcat(pwd, prompt);
+        if ((command_buf = readline(pwd)) == NULL) {
             printf("error reading from stdin");
             return 1;
         }
-        command[strcspn(command, "\n")] = '\0';
+
+        add_history(command_buf);
+        strcpy(command, command_buf);
+        free(command_buf);
+
+        if (strcmp(command, "clear") == 0) {
+            clear();
+        }
+
         getArgs(command, commandarr);
         strcpy(exe, commandarr[0]);
         if (strcmp(exe, "cd") == 0) {
@@ -43,8 +53,9 @@ int main(int argc, char *argv[]) {
         }
 
         else if (strcmp(command, "exit") != 0) {
-			execute(exe, commandarr);
+            execute(exe, commandarr);
         }
+
     }
     return 0;
 }
@@ -54,9 +65,9 @@ int getArgs(char line_in[], char *charray_out[]) {
     char *in_ptr = line_in;
     int i = 0;
     while (i < 32 && (charray_out[i] = strsep(&in_ptr, " ")) != NULL) {
-		i++;
-	}
-	charray_out[i+1] = NULL;
+        i++;
+    }
+    charray_out[i + 1] = NULL;
 
     return i;
 }
@@ -73,11 +84,7 @@ int getArgs(char line_in[], char *charray_out[]) {
 
 int changedir(const char path[]) {
     int result;
-    asm ("syscall" 
-		: "=a"(result) 
-		: "a"(80), "D"(path) 
-		: "rcx", "r11", "memory"
-		);
+    asm("syscall" : "=a"(result) : "a"(80), "D"(path) : "rcx", "r11", "memory");
 
     /* rcx and r11 are always clobbered by the syscall
      * The syscall instruction uses rcx to store the address of the next
@@ -89,29 +96,24 @@ int changedir(const char path[]) {
 }
 
 int getpwd(char *pwd) {
-    asm ("syscall" 
-		: 
-		: "a"(79), "D"(pwd), "S"(64) 
-		: "rcx", "r11", "memory"
-		);
+    asm("syscall" : : "a"(79), "D"(pwd), "S"(64) : "rcx", "r11", "memory");
 
     return 0;
 }
 
-int execute (char *command, char **opts_arr){
-	int exec_status;
-	
-	if (fork() == 0) {
-		exec_status = execvp(command, opts_arr);
+int execute(char *command, char **opts_arr) {
+    int exec_status;
 
-		if (exec_status == -1) {
-			printf("could not execute\n");
-			return 1;
-		}
-	}
-	else {
-		wait(NULL);
-	}
-	printf("\n");
-	return 0;
+    if (fork() == 0) {
+        exec_status = execvp(command, opts_arr);
+
+        if (exec_status == -1) {
+            printf("could not execute\n");
+            return 1;
+        }
+    } else {
+        wait(NULL);
+    }
+    printf("\n");
+    return 0;
 }
